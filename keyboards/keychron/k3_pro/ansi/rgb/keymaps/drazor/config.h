fc52068e37568f81cb74c2ad6dd725543e8025b1
Vagelis Panos <vagelisp.97@gmail.com>
2023-12-22 16:14:22 +0200

    first commit

---
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/drazor/config.h
@@ -0,0 +1,11 @@
+//
+// Created by DrazorV on 22/12/2023.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define CAPS_LOCK_LETTER_LIST \
+{ 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 47, 48 , 49 ,50 ,51 ,52, 53 ,54, 55, 61, 62, 63, 64, 65 ,66 ,67 }
+
+#endif //CONFIG_H
---
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/drazor/rules.mk
@@ -0,0 +1,11 @@
+VIA_ENABLE = yes
+
+RGB_MATRIX_ENABLE = yes
+
+LTO_ENABLE = yes               # link time optimization -- achieves a smaller compiled size
+CONSOLE_ENABLE = yes
+COMMAND_ENABLE = yes
+
+BOOTMAGIC_ENABLE = yes         # Enable Bootmagic Lite
+
+CAPS_WORD_ENABLE = yes         # Enable built-in Caps Word functionality
