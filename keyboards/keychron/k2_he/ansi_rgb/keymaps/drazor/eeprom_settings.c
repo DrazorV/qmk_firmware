9ae79243243568f430b49c483958086456b1a102
Vagelis Panos <vagelisp.97@gmail.com>
2025-01-19 18:39:42 +0200

    initial commit

---
+++ keyboards/keychron/k2_he/ansi_rgb/keymaps/drazor/eeprom_settings.c
@@ -0,0 +1,95 @@
+#include "eeprom_settings.h"
+#include "eeprom.h"
+#include "raw_hid.h"
+#include "print.h"
+
+user_config_t user_config; // Global configuration struct
+
+enum via_value {
+    id_capslock     = 1,
+    id_layer1       = 2,
+    id_layer2       = 3,
+    id_reactive     = 4,
+};
+
+void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
+    // data = [ command_id, channel_id, value_id, value_data ]
+    uint8_t *command_id        = &(data[0]);
+    uint8_t *channel_id        = &(data[1]);
+    uint8_t *value_id_and_data = &(data[2]);
+
+    if ( *channel_id == id_custom_channel ) {
+        switch (*command_id) {
+            case id_custom_set_value:
+            {
+                user_config_set_value(value_id_and_data);
+                break;
+            }
+            case id_custom_get_value:
+            {
+                user_config_get_value(value_id_and_data);
+                break;
+            }
+            case id_custom_save:
+            {
+                eeconfig_update_user_datablock(&user_config); // Save to EEPROM
+                break;
+            }
+            default:
+            {
+                // Unhandled message.
+                *command_id = id_unhandled;
+                break;
+            }
+        }
+        return;
+    }
+
+    *command_id = id_unhandled;
+}
+
+
+// VIA Command Handling
+void  user_config_set_value(uint8_t *data) {
+
+    uint8_t *value_id = &(data[0]);  // Get the value ID
+    HSV *hsv_data   = (HSV *)&(data[1]);  // Point to HSV structure
+
+    switch (*value_id) {
+        case id_capslock:
+            user_config.caps_lock_hs = *hsv_data;
+            break;
+        case id_layer1:
+            user_config.layer1_hs = *hsv_data;
+            break;
+        case id_layer2:
+            user_config.layer2_hs = *hsv_data;
+            break;
+        case id_reactive:
+            user_config.reactive_overlay = data[1];
+        default:
+            return; // Unknown value ID, do nothing
+    }
+}
+
+void user_config_get_value(uint8_t *data) {
+    // data = [ value_id, R, G, B ]
+    uint8_t *value_id = &(data[0]);
+    HSV *hsv_data   = (HSV *)&(data[1]);
+
+    switch (*value_id) {
+        case id_capslock:
+            *hsv_data = user_config.caps_lock_hs;
+            break;
+        case id_layer1:
+            *hsv_data = user_config.layer1_hs;
+            break;
+        case id_layer2:
+            *hsv_data = user_config.layer2_hs;
+            break;
+        case id_reactive: // Handle overlay toggle as boolean
+            data[1] = user_config.reactive_overlay;
+            break;
+    }
+}
+
---
+++ keyboards/keychron/k2_he/ansi_rgb/keymaps/drazor/eeprom_settings.h
@@ -0,0 +1,28 @@
+#ifndef EEPROM_SETTINGS_H
+#define EEPROM_SETTINGS_H
+
+#include "quantum.h"
+
+// Define the User Config Union
+typedef union {
+    uint8_t raw[EECONFIG_USER_DATA_SIZE];
+    struct {
+        HSV caps_lock_hs; // Hue & Saturation for Caps Lock
+        HSV layer1_hs;    // Hue & Saturation for Layer 1
+        HSV layer2_hs;    // Hue & Saturation for Layer 2
+        bool reactive_overlay; // Toggle for the reactive overlay effect
+    } __attribute__((packed)); // Ensure no padding
+} user_config_t;
+
+extern user_config_t user_config;
+
+
+// Global Variable
+extern user_config_t user_config;
+
+// VIA Command Handling
+void via_custom_value_command_kb(uint8_t *data, uint8_t length);
+void user_config_set_value(uint8_t *data);
+void user_config_get_value(uint8_t *data);
+
+#endif // EEPROM_SETTINGS_H
